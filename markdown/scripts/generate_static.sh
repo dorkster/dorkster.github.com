a5c5e8a16b18b9b2c0178b1c235bb73bd45e1274
Justin Jacobs <jajdorkster@gmail.com>
2015-08-23 13:51:46 -0400

    Remove PAGEDESC and PAGEIMG from static pages

--- markdown/scripts/generate_static.sh
+++ markdown/scripts/generate_static.sh
@@ -3,17 +3,20 @@
 # this script generates Projects, About, etc.
 cd ~/Projects/dorkster.github.com/markdown/
 
+PAGEDESC=""
+PAGEIMG=""
+
 for i in *.md; do
     if [ "$i" == "index.md" ]; then
         PAGETITLE="Home"
     else
         # this assumes that pages start with "## PAGETITLE"
-        PAGETITLE=$(head -n 1 $i | cut -c 4- | sed -e 's/\//\\\//g')
+        PAGETITLE=$(head -n 1 $i | cut -c 4- | sed -e 's/[\/&]/\\&/g')
     fi
 
     HTMLFILE="$(basename $i ".md").html"
     echo "Generating ../$HTMLFILE"
-    cat html_template/header.txt | sed -e "s/PAGETITLE/$PAGETITLE/g" > "../$HTMLFILE"
+    cat html_template/header.txt | sed -e "s/PAGETITLE/$PAGETITLE/g" -e "s/PAGEDESC/$PAGEDESC/g" -e "s/PAGEIMG/$PAGEIMG/g" > "../$HTMLFILE"
 
     # highlight the matching page link in nav
     CURRENT=$(sed -n "/$HTMLFILE/=" ../$HTMLFILE | head -n1)
